6a9a2cc13cb1c1baa4a3907341a5011b0866f2cd
Tom Jackson <tom.s.jackson090@gmail.com>
2021-03-04 12:26:22 +0000

    setting universal timeout

--- scrapers.js
+++ scrapers.js
@@ -12,7 +12,8 @@ async function scrapeChannel(url, techInput) {
     'Accept-Language': 'en-US,en;q=0.9'
   });
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
-  await page.goto(url, {timeout: 60000, waitUntil: 'domcontentloaded'});
+  await page.goto(url, {  waitUntil: 'networkidle2',timeout: 0});
+  page.setDefaultTimeout(0); 
   
   try{
     await page.waitForSelector('.PUpOsf');
